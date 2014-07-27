f5e2719186bc9ee99fb7408d820153df7850c45c
Nick Reid <nickreid@nickreid.com>
2014-07-27 12:19:21 -0400

    added basic admin

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from films.models import Film
 # Register your models here.
+
+class FilmAdmin(admin.ModelAdmin):
+    date_hierarchy = 'release_date'
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'year', 'release_date', 'duration')
+        }),
+        ("Notes", {
+            'fields': ('description',)
+        }),
+    )
+
+admin.site.register(Film, FilmAdmin)
