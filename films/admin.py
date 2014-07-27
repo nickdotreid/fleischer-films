8f1ad9ab39261ae88e8ea628662990d3d0ae3bb0
Nick Reid <nickreid@nickreid.com>
2014-07-27 12:39:09 -0400

    added links to admin interface

--- films/admin.py
+++ films/admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
 
-from films.models import Film
+from films.models import Film, Tag, LinkType, Link
 # Register your models here.
+
+class LinkInline(admin.TabularInline):
+    model = Link
 
 class FilmAdmin(admin.ModelAdmin):
     date_hierarchy = 'release_date'
 
     fieldsets = (
         (None, {
-            'fields': ('title', 'year', 'release_date', 'duration')
+            'fields': ('title', ('year', 'release_date'), 'duration')
         }),
         ("Notes", {
             'fields': ('description',)
         }),
     )
+
+    inlines = [
+    	LinkInline,
+    ]
 
 admin.site.register(Film, FilmAdmin)
