0a99597f26cf39957b1971509fa34c9081df4652
Nick Reid <nickreid@nickreid.com>
2014-08-20 13:38:56 -0400

    added series list view

--- films/urls.py
+++ films/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('films.views',
+	url(r'^series/(?P<series_id>\w+)','series'),
 	url(r'^','list_series'),
 )
--- films/views.py
+++ films/views.py
@@ -5,14 +5,17 @@ import json
 
 from django.template import RequestContext
 
+from films.models import Series, Film
+
 def list_series(request):
+	return render_to_response('series/list.html',{
+		'series':Series.objects.all(),
+        }, context_instance=RequestContext(request))
+
+def series(request, series_id):
 	return render_to_response('base.html',{
         }, context_instance=RequestContext(request))
 
-def view_series(request, series_id):
-	return render_to_response('base.html',{
-        }, context_instance=RequestContext(request))
-
-def view_film(request, film_id):
+def film(request, film_id):
 	return render_to_response('base.html',{
         }, context_instance=RequestContext(request))
