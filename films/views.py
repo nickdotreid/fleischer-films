42e10206d1db74bd01cb65e6957f456c799a5e30
Nick Reid <nickreid@nickreid.com>
2014-08-20 13:24:56 -0400

    added stubs for views

--- films/views.py
+++ films/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+import json
 
-# Create your views here.
+from django.template import RequestContext
+
+def list_series(request):
+	return render_to_response('base.html',{
+        }, context_instance=RequestContext(request))
+
+def view_series(request, series_id):
+	return render_to_response('base.html',{
+        }, context_instance=RequestContext(request))
+
+def view_film(request, film_id):
+	return render_to_response('base.html',{
+        }, context_instance=RequestContext(request))
