903f5b5fcaeab444ee69b8e2837f1323141bcace
Nick Reid <nickreid@nickreid.com>
2014-08-31 21:38:01 -0400

    got styles to show up

--- fleischer_films/settings/production.py
+++ fleischer_films/settings/production.py
@@ -179,6 +179,8 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
     'djangobower.finders.BowerFinder',
     )
 
